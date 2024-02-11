50a15510e8d5e2d5f2a096bcbc7f29fcf874e2b8
omer-dan <116800631+omer-dan@users.noreply.github.com>
2024-02-11 17:39:28 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function findDifference() {
-    var errorText = document.getElementById("inputText").value;
+    var errorText = document.getElementById("errorText").value;
 
     var startIndex = errorText.indexOf("The output for test") + "The output for test".length;
     var endIndex = errorText.indexOf("did not match");
